4481acd24975926cbb10947ded3c742c34d699a0
Stephen Daniel <swd@pobox.com>
2020-11-14 08:27:23 -0500

    fixed up the automation service to understand that the alarm state sensor now conforms to homie v3

--- automation/main.go
+++ automation/main.go
@@ -57,17 +57,72 @@ var alarmStates = map[string]int{
 	"alarmed-burglar": 5,
 	"alarmed-fire":    5,
 	"armed-away":      1,
+	"unknown":         0,
 }
 
-var r1handler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+const (
+	DET_UNKNOWN int = 1 + iota
+	DET_ONLINE
+	DET_OFFLINE
+)
+
+var detState int = DET_UNKNOWN
+var detStateDetailed string = "unknown"
+var lastAlarmState string = "unknown"
+
+var stateMap = map[string]int{
+	"init":         DET_OFFLINE,
+	"ready":        DET_ONLINE,
+	"disconnected": DET_OFFLINE,
+	"sleeping":     DET_OFFLINE,
+	"lost":         DET_OFFLINE,
+	"alert":        DET_OFFLINE,
+}
+
+// Since both r1a and r1b can happen simultaneously there are race conditions her.
+// Right thing to do would be to push the detector state changes and the alarm state
+// changes to a go routine to serialize them. Maybe next version.
+
+// Changes in the alarm state come here.
+var r1ahandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	payload := string(msg.Payload())
 
 	if _, ok := alarmStates[payload]; ok {
-		client.Publish("environment/alarm-state", 0, true, payload)
+		if detState == DET_ONLINE {
+			client.Publish("environment/alarm-state", 0, true, payload)
+		}
 		logMessage("Alarm state: " + payload)
+		lastAlarmState = payload
 	} else {
 		logMessage("Invalid device alarm state: " + payload)
 	}
+}
+
+// Changes in whether the alarm detector is on line come here
+var r1bhandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	var newState int
+
+	payload := string(msg.Payload())
+
+	if detStateDetailed != payload {
+		detStateDetailed = payload
+		logMessage("Alarm Detector online state: " + payload)
+	}
+
+	newState, ok := stateMap[payload]
+	if !ok {
+		newState = DET_UNKNOWN
+	}
+
+	if newState != detState {
+		detState = newState
+		if newState == DET_ONLINE {
+			client.Publish("environment/alarm-state", 0, true, lastAlarmState)
+		} else {
+			client.Publish("environment/alarm-state", 0, true, "unknown")
+		}
+	}
+
 }
 
 var r23handler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
@@ -120,7 +175,12 @@ func main() {
 
 	logMessage("home automation daemon started")
 
-	if token := client.Subscribe("devices/alarm-state-0001/alarm-state/state", 0, r1handler); token.Wait() && token.Error() != nil {
+	if token := client.Subscribe("devices/alarm-state-0001/alarm-state/state", 0, r1ahandler); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	if token := client.Subscribe("devices/alarm-state-0001/$state", 0, r1bhandler); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
