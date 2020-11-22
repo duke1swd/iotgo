1c8179a02ab3e08e4e362e8b52de74451747d824
Stephen Daniel <swd@pobox.com>
2020-11-22 13:17:43 -0500

    Still typing in code. Compiles,but not yet finished.

--- envlogger/main.go
+++ envlogger/main.go
@@ -1,11 +1,12 @@
 /*
  * Log the environment.
  * Intent is to create a long term data capture of the environment for later analysis.
-*/
+ */
 
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -20,19 +21,43 @@ import (
 const defaultLogDirectory = "/var/log"
 const defaultLogFileName = "HomeEnvironment"
 const myName = "Environment Logger"
-const defaultLogInterval = 15	// minutes
+const defaultLogInterval = 15 // minutes
+
+type stateUpdateType struct {
+	name  string
+	state string
+}
+
+type valueUpdateType struct {
+	name  string
+	value float64
+}
 
 var (
 	logDirectory    string
 	fullLogFileName string
-	logInterval time.Duration
+	logInterval     time.Duration
+	updateChannel   chan interface{}
 )
 
-var stateMap = map[string]bool {
+var stateMap = map[string]bool{
 	"alarm-state": true,
 }
 
+var sensorMap = map[string]bool{
+	"outdoor-temp": true,
+	"outdoor-lux":  true,
+}
+
+type sensorDataType struct {
+	sumValues float64
+	numValues int
+}
+
+var sensorData map[string]sensorDataType
+
 // Process the environment
+// Initialize a few data structures
 func init() {
 	var i int64
 
@@ -58,8 +83,15 @@ func init() {
 		i = j
 	}
 	logInterval = time.Duration(i) * time.Minute
+
+	updateChannel = make(chan interface{})
+	sensorData = make(map[string]sensorDataType)
 }
 
+/*
+ * This handles messages from the mqtt client.
+ * I believe that multiple instances of this can be called on different threads
+ */
 var handler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 	topic := string(msg.Topic())
 	payload := string(msg.Payload())
@@ -78,8 +110,94 @@ var handler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
 
 	// Is this a known state variable?
 	if _, ok := stateMap[sensor]; ok {
-		dataLog(sensor, payload)
+		var update stateUpdateType
+		update.name = sensor
+		update.state = payload
+		updateChannel <- update
 		return
+	}
+
+	// Is this a known sensor?
+	if _, ok := sensorMap[sensor]; ok {
+		var update valueUpdateType
+		update.name = sensor
+		v, err := strconv.ParseFloat(payload, 64)
+		if err != nil {
+			return
+		}
+		update.value = v
+		updateChannel <- update
+		return
+	}
+
+	// If we don't know what this is, do nothing.
+}
+
+/*
+ * This routine returns a context that will timeout when we are next
+ * supposed to dump sensor data to the log.  The timeouts are naturally
+ * aligned.  For example, if we are doing timeouts once an hour, then they will
+ * occur on the hour.  Alignments occur relative to last midnight.
+ *
+ * The global variable logInterval controls how often we do this.
+ */
+func heartbeat(con context.Context) context.Context {
+	// First, compute when last midnight was
+	year, month, date := time.Now().Date()
+	loc, _ := time.LoadLocation("Local")
+	lastMidnight := time.Date(year, month, date, 0, 0, 0, 0, loc)
+	// Now, how many intervals since then
+	ni := time.Since(lastMidnight) / logInterval
+	// One more interval until the deadline
+	c, _ := context.WithDeadline(con, lastMidnight.Add((ni + 1) * logInterval))
+	return c
+}
+
+/*
+ * This routine runs as its own thread, recieving updates from the
+ * message handler and generating log messages as appropriate.
+ * It also uses contexts to generate a periodic heartbeat for dumping
+ * sensor averages to the log.
+ */
+func updater(con context.Context, client mqtt.Client) {
+	timeoutContext := heartbeat(con)
+
+	// Loop forever getting updates and heartbeats
+	for {
+		select {
+		case data := <-updateChannel:
+			switch update := data.(type) {
+			case stateUpdateType:
+				// For state variables, just log their changed state
+				dataLogger(update.name, update.state)
+			case valueUpdateType:
+				// For sensor variables, we average them until the next heartbeat
+				var s sensorDataType
+				s, ok := sensorData[update.name]
+				if !ok {
+					s.sumValues = 0
+					s.numValues = 0
+				}
+				s.sumValues += update.value
+				s.numValues++
+				sensorData[update.name] = s
+			}
+		case <- timeoutContext.Done():
+			// reset the heartbeat context
+			timeoutContext = heartbeat(con)
+
+			// Loop over all known sensors, outputing stuff and zeroing out
+			for name, data := range(sensorData) {
+				if data.numValues <= 0 {
+					dataLogger(name, "no-data")
+				} else {
+					dataLogger(name, strconv.FormatFloat(data.sumValues, 'f', 1, 64))
+				}
+				data.numValues = 0
+				data.sumValues = 0.
+				sensorData[name] = data
+			}
+		}
 	}
 }
 
