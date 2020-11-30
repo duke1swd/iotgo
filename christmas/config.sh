4c5cbdf6b517d8e775f2b84759e6b559f28e91af
Stephen Daniel <swd@pobox.com>
2020-11-30 15:34:08 -0500

    Minor change to config.sh.  Added a second region.

--- christmas/config.sh
+++ christmas/config.sh
@@ -10,6 +10,9 @@ christmas/season/start 11/1
 christmas/season/end 1/6
 christmas/enable true
 christmas/indoor/window-start light
-christmas/indoor/window-end 1:00
+christmas/indoor/window-end 23:00
 christmas/indoor/devices plug-0001
+christmas/outdoor/window-start light
+christmas/outdoor/window-end 23:00
+christmas/outdoor/devices plug-0002
 EOF
