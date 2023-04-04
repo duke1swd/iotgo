07abd2e845eb8b97197fc2f12175f896c4f43ebd
Stephen Daniel <swd@pobox.com>
2023-04-04 09:01:28 -0400

    Random changes

--- lighting/oneCommand.sh
+++ lighting/oneCommand.sh
@@ -4,8 +4,9 @@ do
 	echo mosquitto_pub -r -t ${line[0]} -m ${line[1]}
 	mosquitto_pub -r -t ${line[0]} -m ${line[1]}
 done << EOF
-lighting/outdoor/command off
+lighting/tree/command off
 EOF
+#environment/outdoor-light 3
 #devices/plug-0002/button/button true
 #lighting/enable true
 #lighting/test-region/devices plug-fake
